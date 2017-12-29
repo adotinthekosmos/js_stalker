118ee7b37ff54ed9cda8a9e69897b4fbe261a611
khoapa <khoateamer@gmail.com>
2017-12-29 12:01:07 +0700

    final commit

---
+++ script.js
@@ -0,0 +1,7 @@
+fName = prompt("Hi sensei, what's your first name");
+lName = prompt("And your last name ?");
+age = prompt("Age please!");
+alert("Arigato gozaimastu!");
+
+console.log(`Your full name is ${fName} ${lName}`);
+console.log(`Your age is ${age}`)
